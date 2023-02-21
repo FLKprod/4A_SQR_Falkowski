39d2ccf1522b7a5a397afa4c84480dcfe8df4317
lucas-b700 <65290583+lucas-b700@users.noreply.github.com>
2023-02-21 10:33:44 +0000

    Modification redis_test.py

--- redis_test.py
+++ redis_test.py
@@ -1,12 +1,12 @@
 import redis
-#pool = redis.ConnectionPool(host='localhost',port=6379,db=0)
 
-r = redis.StrictRedis(host='localhost',port=6379,db=0)
+r = redis.Redis(host='localhost',port=6379,db=0)
 r.set('foo','bar')  # ajout de variable et true en retour si reussite
 r.get('foo') # lecture de la valeur de foo
 
 # test Pipelines #
 pipe = r.pipeline()
+
 pipe.set('foo',5)
 pipe.set('bar',18.5)
 pipe.set('blee',"Message")
