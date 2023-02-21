7a74a80c724632d2c377be5f51e313c4ef696bb6
Maxou <105202869+FLKprod@users.noreply.github.com>
2023-02-21 11:14:21 +0100

    fichiers WEB

---
+++ index.js
@@ -0,0 +1,6 @@
+function validate(){
+    var nombre1 = document.getElementById("nombre1").value;
+    var nombre2 = document.getElementById("nombre2").value;
+    var symbol = document.getElementById("symbol").value;
+    alert("calcul de "+nombre1 + " " + symbol + " " + nombre2 + " en cours...")
+}   
