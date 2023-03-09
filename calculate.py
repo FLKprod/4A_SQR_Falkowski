747ced3b36d8be2c83882f2128317acdc807567f
Maxou <105202869+FLKprod@users.noreply.github.com>
2023-03-09 09:30:37 +0000

    fichiers web twitter

--- calculate.py
+++ calculate.py
@@ -1,28 +1,24 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 resultat = {}
 
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    data = request.get_json()
-    operator = data['operator']
-    num1 = data['num1']
-    num2 = data['num2']
-
-    if operator == '+':
-        resultatOp = num1 + num2
-    elif operator == '-':
-        resultatOp = num1 - num2
-    elif operator == '*':
-        resultatOp = num1 * num2
-    elif operator == '/':
-        resultatOp = num1 / num2
-
+@app.route('/calculate/<nombre1>/<nombre2>/<symbol>', methods=['POST'])
+def calculate(nombre1,nombre2,symbol):
+    if symbol == '+':
+        resultatOp = nombre1 + nombre2
+    elif symbol == '-':
+        resultatOp = nombre1 - nombre2
+    elif symbol == '*':
+        resultatOp = nombre1 * nombre2
+    elif symbol == '/':
+        resultatOp = nombre1 / nombre2
     resultat_id = len(resultat) + 1
     resultat[resultat_id] = resultatOp
-    return jsonify({'resultat_id': resultat_id})
+    return str(resultatOp)
 
 
 if __name__ == '__main__':
     app.run()
+    
--- index.js
+++ index.js
@@ -2,5 +2,20 @@ function validate(){
     var nombre1 = document.getElementById("nombre1").value;
     var nombre2 = document.getElementById("nombre2").value;
     var symbol = document.getElementById("symbol").value;
-    alert("calcul de "+nombre1 + " " + symbol + " " + nombre2 + " en cours...")
-}   
+    var json = '{"nombre1":'+ nombre1+ ',"nombre2":'+ nombre2+ ',"symbol":'+ symbol+ '}'
+    nombre1=parseInt(nombre1);
+    nombre2=parseInt(nombre2)
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST","http://localhost/calculate/"+nombre1+"/"+nombre2+"/"+symbol);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onload = () => {
+      alert("calcul de "+nombre1 + " " + symbol + " " + nombre2 + " = " + xhr.response)
+      if (xhr.status == 200) {
+        data = xhr.response;
+        
+      } else {
+        console.log(`Error: ${xhr.status}`);
+      }
+    };
+    xhr.send(json);
+}  
