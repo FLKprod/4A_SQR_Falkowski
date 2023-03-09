cb9620f77cde45d3b37dd21fbf9d61d4087308f5
Maxou <105202869+FLKprod@users.noreply.github.com>
2023-03-09 10:53:24 +0000

    probleme don commit

--- Twitter/backend/twitter.py
+++ Twitter/backend/twitter.py
@@ -1,14 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
-resultat = {}
-
 
 class Tweet:
-        def __init__(self,profil,corps):
-            self.profil = profil
-            self.corps = corps
+    def __init__(self,profil,corps):
+        self.profil = profil
+        self.corps = corps
 @app.route('/tweeter/<profil>/<corps>', methods=['POST'])
 def tweeter(profil,corps):
     return Tweet(profil,corps)
--- Twitter/frontend/main.js
+++ Twitter/frontend/main.js
@@ -1,8 +1,22 @@
 function tweeter(){
     var corps = document.getElementById("corps").value;
-    var profil = "falko"
+    var profil = localStorage.getItem("username")
     var json = '{"profil":'+ profil + ',"corps":'+ corps + '}'
-    fetch('http://localhost:5000/tweeter/'+profil+'/'+corps).then(function(response) {
-      return response.blob();
-    });
-}  
+
+    fetch('http://localhost:5000/tweeter/'+profil+'/'+corps,
+    {
+        method: "POST", 
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then( response => response.json() )
+    .then( data => console.log(data) )
+}
+
+function changeuser(){
+  localStorage.setItem("username",document.getElementById("connect"))
+  alert("username modifié")
+  document.getElementById("user-connected").innerText(localStorage.getItem("username"))
+}
