7340c4fb8cc332809e5999731a79958aca8ee813
Maxou <105202869+FLKprod@users.noreply.github.com>
2023-03-14 09:37:09 +0000

    frontend et backend

--- Twitter/frontend/main.js
+++ Twitter/frontend/main.js
@@ -1,25 +1,26 @@
 document.getElementById("user-connected").innerText =localStorage.getItem("username")+" est actuellement connécté"
 
 function tweeter(){
+    server.listen(3001, 'localhost'); // or server.listen(3001, '0.0.0.0'); for all interfaces
+    server.on('listening', function() {
+        console.log('Express server started on port %s at %s', server.address().port, server.address().address);
+});
     if(document.getElementById("corps").value.length != 0){
         var corps = document.getElementById("corps").value;
-        var profil = localStorage.getItem("username")
-        var json = '{"profil":'+ profil + ',"corps":'+ corps + '}'
-
-        fetch('http://localhost:5000/tweeter/'+profil+'/'+corps,
-        {
-            method: "POST", 
-            mode: 'no-cors',
-            headers: {
-                'Content-Type': 'application/json',
-        }
-        })
-        .then( response => response.json() )
-        .then( data => console.log(data) )
-    }
-    else{
-        alert("tweet vide..")
-    }
+        var profil = localStorage.getItem("username");
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST","http://localhost/calculate/"+profil+"/"+corps);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onload = () => {
+            if (xhr.status == 200) {
+                alert("tweeté ! : "+xhr.response)
+            }
+            else {
+                console.log(`Error: ${xhr.status}`);
+            }
+        };
+        xhr.send("ok");
+    }  
 }
 
 function changeuser(){
