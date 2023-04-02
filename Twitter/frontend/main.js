62c9d57b9202c24c30fba6de11f7c2e99a43f3cd
Maxou <105202869+FLKprod@users.noreply.github.com>
2023-04-02 15:20:40 +0200

    Add files via upload

--- Twitter/frontend/main.js
+++ Twitter/frontend/main.js
@@ -9,10 +9,45 @@ function tweeter(){
         xhr.open("POST","http://127.0.0.1:5000/tweeter/"+profil+"/"+corps);
         xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-        xhr.setRequestHeader("Origin", "https://lucas-b700-expert-giggle-rw7gwvwqq44c5jg4-8080.preview.app.github.dev/");
         xhr.onload = () => {
             if (xhr.status == 200) {
-                alert("tweeté ! : "+xhr.response)
+                alert("c'est tweeté ! : ");
+                var newtweet = document.createElement("div");
+                newtweet.setAttribute("id", "tweet")
+                document.getElementById("tweets").appendChild(newtweet)
+
+                var newuser = document.createElement("div");
+                newuser.setAttribute("id", "user")
+                newtweet.appendChild(newuser)
+
+                var newuserphoto =  document.createElement("img");
+                newuserphoto.setAttribute("id", "photo-auteur")
+                newuserphoto.src = "user-photo.png";
+                newuser.appendChild(newuserphoto)
+
+                var newusername = document.createElement("p");
+                newusername.setAttribute("id", "auteur")
+                newusername.innerText = profil;
+                newuser.appendChild(newusername)
+
+                var newtweetcorps = document.createElement("p");
+                newtweetcorps.setAttribute("id", "corps")
+                newtweetcorps.innerText = corps;
+                newtweet.appendChild(newtweetcorps)
+
+                var buttons = document.createElement("div");
+                buttons.setAttribute("class", "buttons")
+                newtweet.appendChild(buttons)
+
+                var likebutton = document.createElement("button");
+                likebutton.setAttribute("id", "like")
+                likebutton.innerText = "❤️";
+                buttons.appendChild(likebutton)
+
+                var retweetbutton = document.createElement("button");
+                retweetbutton.setAttribute("id", "retweet")
+                retweetbutton.innerText = "🔁";
+                buttons.appendChild(retweetbutton)
             }
             else {
                 console.log(`Error: ${xhr.status}`);
---
+++ Twitter/frontend/user.js
@@ -0,0 +1,2 @@
+document.getElementById("user-connected").innerText =localStorage.getItem("username");
+document.getElementById("action").innerText= localStorage.getItem("username") + " à tweeté";
