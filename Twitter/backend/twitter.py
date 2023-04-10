436fadbaf8e958e65e96fcc4b0bc45d8a29a0b1f
Maxou <105202869+FLKprod@users.noreply.github.com>
2023-04-10 12:10:32 +0200

    ajout des filtres en fonction du profil et du sujet

--- Twitter/backend/twitter.py
+++ Twitter/backend/twitter.py
@@ -2,7 +2,7 @@
 # python -m flask run
 # docker run -p 6379:6379 redis
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
 import sys
@@ -31,15 +31,22 @@ class Tweet:
         
     def __json__(self):
         return {"profil": self.profil, "corps": self.corps, "sujet" : self.sujet, "id": self.id}
+    
+    def getprofil(self):
+        return self.profil
+
 
 # route pour tweeter
 @app.route('/tweeter/<profil>/<corps>/<sujet>/<int:id>', methods=['POST'])
 def tweeter(profil,corps,sujet,id):
     r.set(id, json.dumps(Tweet(profil,corps,sujet,id).__json__()))
     print(r.get(id))
-    print("deded")
     sys.stdout.flush()
     return Tweet(profil,corps,sujet,id).__json__()
+
+
+
+
 
 # route pour ajouter un utilisateur
 @app.route('/user/<profil>/<int:id>', methods=['POST'])
@@ -55,36 +62,62 @@ def changeuser(profil,id):
         print(prof)
         if profil == prof:
             user_unique = i+1
-    if user_unique == -1:
-        id = int(user_id) + 1
-        r.set("user_id", id)
-        r.set(id, json.dumps(User(profil,id).__json__()))
-    else:
-        id = user_unique
+        if user_unique == -1:
+            id = int(user_id) + 1
+            r.set("user_id", id)
+            r.set(id, json.dumps(User(profil,id).__json__()))
+        else:
+            id = user_unique
     print(r.get(id))
-    print("deded")
     sys.stdout.flush()
     return User(profil,id).__json__()
 
 
-# route pour recuperer les tweets
-@app.route('/tweeter/<profil>', methods=['GET'])
-def get_all_tweets(profil):
-    tweets_user = []
-    #for i in r.get('tweets').__len__:
-    #    if r.get('tweets')[i].__getattribute__("profil")==profil:
-    #        tweets_user.__add__(r.get('tweets')[i])
-    return json.loads(tweets_user)
 
 
-# route pour recuperer tous les tweets
-@app.route('/tweeter', methods=['GET'])
-def get_tweets(profil):
-    all_tweets = []
-    for i in r.get('tweets').__len__:
-        all_tweets.__add__(r.get('tweets')[i])
-    return json.dumps(all_tweets)
 
-if __name__ == '__main__':
-    app.run()
+# route pour recuperer les tweets d'un profil
+@app.route('/<profil>', methods=['POST'])
+def get_tweets_by_profil(profil):
+    user_tweet = []
+    for key in r.scan_iter("*"):
+        tweetSTR=r.get(key).decode('utf-8')
+        tweet=json.loads(tweetSTR)
+        if 'sujet' in tweetSTR and tweet['profil']==profil:
+            print(tweet['profil'])
+            user_tweet.append(tweet)
+        else:
+            print("pas un tweet maos osef")
+    print(user_tweet)
+    return user_tweet
 
+# route pour recuperer les tweets en fonction d'un sujet
+@app.route('/<sujet>', methods=['POST'])
+def get_tweets_by_theme(sujet):
+    user_tweet = []
+    for key in r.scan_iter("*"):
+        tweetSTR=r.get(key).decode('utf-8')
+        tweet=json.loads(tweetSTR)
+        if 'sujet' in tweetSTR and tweet['sujet']==sujet:
+            profil=tweet['profil']
+            print(profil)
+            user_tweet.append(tweet)
+        else:
+            print("pas un tweet maos osef")
+    print(user_tweet)
+    return user_tweet
+
+
+# route pour recuperer tous les tweets dans l'API
+@app.route('/all_tweets', methods=['POST'])
+def get_all_tweets():
+    user_tweet = []
+    for key in r.scan_iter("*"):
+        tweetSTR=r.get(key).decode('utf-8')
+        tweet=json.loads(tweetSTR)
+        if 'sujet' in tweetSTR:
+            user_tweet.append(tweet)
+        else:
+            print("pas un tweet maos osef")
+    print(user_tweet)
+    return user_tweet
