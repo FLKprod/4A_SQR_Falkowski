71133c720909028b3271ed21ad874f3adf8e4825
Maxou <105202869+FLKprod@users.noreply.github.com>
2023-04-10 12:11:08 +0200

    filtre sujet fonctionne pas mais le reste si

--- Twitter/frontend/main.js
+++ Twitter/frontend/main.js
@@ -1,10 +1,63 @@
 document.getElementById("user-connected").innerText =localStorage.getItem("username")+" est actuellement connécté"
-document.getElementById("sujet-selected").value =localStorage.getItem("sujet");
+if (localStorage.getItem("sujet") == null){
+    localStorage.setItem("sujet","Globale")
+}
+else{
+    document.getElementById("theme-selected").innerHTML =localStorage.getItem("sujet");
+}
+var xhr = new XMLHttpRequest();
+xhr.open("POST","http://127.0.0.1:5000/all_tweets");
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+xhr.onload = () => {
+    if (xhr.status === 200) {
+    var tweets = JSON.parse(xhr.response)
+    for(i=0;i<tweets.length;i++){
+            var newtweet = document.createElement("div");
+            newtweet.setAttribute("id", "tweet")
+            document.getElementById("tweets").appendChild(newtweet)
+
+            var newuser = document.createElement("div");
+            newuser.setAttribute("id", "user");
+            newtweet.appendChild(newuser)
+
+            var newuserphoto =  document.createElement("img");
+            newuserphoto.setAttribute("id", "photo-auteur")
+            newuserphoto.src = "user-photo.png";
+            newuser.appendChild(newuserphoto)
+
+            var newusername = document.createElement("p");
+            newusername.setAttribute("id", "auteur")
+            newusername.innerText = tweets[i].profil;
+            newuser.appendChild(newusername)
+
+            var newtweettheme = document.createElement("p");
+            newtweettheme.setAttribute("id", "sujet")
+            newtweettheme.innerText = "#"+tweets[i].sujet;
+            newuser.appendChild(newtweettheme)
+
+            var newtweetcorps = document.createElement("p");
+            newtweetcorps.setAttribute("id", "corps")
+            newtweetcorps.innerText = tweets[i].corps;
+            newtweet.appendChild(newtweetcorps)
+        }
+    }
+    else {
+        console.log(`Error: ${xhr.status}`);
+    }
+};
+xhr.send();
+
+
+
+
+
+
+
+
 function tweeter(){
-   
     if(document.getElementById("corps").value.length != 0){
         var id = Date.now();
-        console.log("id:", id);
         var corps = document.getElementById("corps").value;
         var profil = localStorage.getItem("username");
         var sujet = localStorage.getItem("sujet");
@@ -14,7 +67,7 @@ function tweeter(){
         xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
         xhr.onload = () => {
             if (xhr.status == 200) {
-                alert("c'est tweeté ! : ");
+                alert("c'est tweeté !");
                 var newtweet = document.createElement("div");
                 newtweet.setAttribute("id", "tweet")
                 document.getElementById("tweets").appendChild(newtweet)
@@ -70,38 +123,89 @@ function tweeter(){
 function changeuser(){
     if(document.getElementById("connect-user").value.length != 0){
         localStorage.setItem("username",document.getElementById("connect-user").value)
-        document.getElementById("user-connected").innerText = localStorage.getItem("username") + " est actuellement connecté"
+        document.getElementById("user-connected").innerText = localStorage.getItem("username") + " est actuellement connecté";
         var xhr = new XMLHttpRequest();
-        var id = 0;
-        var profil = localStorage.getItem("username");
+        var id = Date.now();
+        var profil = localStorage.getItem("username")
         xhr.open("POST","http://127.0.0.1:5000/user/"+profil+"/"+id);
         xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-        xhr.onload = () => { 
+        xhr.setRequestHeader("Access-Control-Allow-Origin","*");
+        xhr.onload = () => {
             if (xhr.status == 200) {
-                alert("username modifié")
-                console.log(xhr.responseText)
+                alert("ca marche regarde : "+xhr.response)
             }
             else {
+                alert("marche po")
                 console.log(`Error: ${xhr.status}`);
             }
         };
-        xhr.send();
+        xhr.send("ok");
     }
     else{
         alert("veuillez saisir un utilisateur")
-    }
+    }   
 }
 
 function changetheme(){
     localStorage.setItem("sujet",document.getElementById("select-theme").value)
-}
+    alert('sujet modifié')
+    var option = document.createElement("option");
+    option.innerHTML=document.getElementById("select-theme").value
+    document.getElementById("all-themes").appendChild(option)
 
-function selecttheme(){
-    localStorage.setItem("sujet",document.getElementById("sujet-selected").value)
+    let elements = document.getElementsByClassName("tweets");
+    for (let i = 0; i < elements.length; i++) {
+        var sujetElement = elements[i].querySelector("#sujet");
+        if(sujetElement==localStorage.getItem("sujet"))
+            elements[i].style.display = "none";
+    }
+    var sujet = localStorage.getItem('sujet')
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST","http://127.0.0.1:5000/"+sujet);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            var tweets = JSON.parse(xhr.response)
+            for(let i=0;i<tweets.length;i++){
+                if(tweets[i].sujet==localStorage.getItem("sujet")){
+                    var newtweet = document.createElement("div");
+                    newtweet.setAttribute("id", "tweet")
+                    newtweet.style.display='';
+                    document.getElementById("tweets").appendChild(newtweet)
+
+                    var newuser = document.createElement("div");
+                    newuser.setAttribute("id", "user");
+                    newtweet.appendChild(newuser)
+
+                    var newuserphoto =  document.createElement("img");
+                    newuserphoto.setAttribute("id", "photo-auteur")
+                    newuserphoto.src = "user-photo.png";
+                    newuser.appendChild(newuserphoto)
+
+                    var newusername = document.createElement("p");
+                    newusername.setAttribute("id", "auteur")
+                    newusername.innerText = tweets[i].profil;
+                    newuser.appendChild(newusername)
+
+                    var newtweettheme = document.createElement("p");
+                    newtweettheme.setAttribute("id", "sujet")
+                    newtweettheme.innerText = "#"+tweets[i].sujet;
+                    newuser.appendChild(newtweettheme)
+
+                    var newtweetcorps = document.createElement("p");
+                    newtweetcorps.setAttribute("id", "corps")
+                    newtweetcorps.innerText = tweets[i].corps;
+                    newtweet.appendChild(newtweetcorps)
+                }
+            }
+        }
+        else {
+            console.log(`Error: ${xhr.status}`);
+        }
+    };
+    xhr.send();
 }
-sujet_selected=document.getElementById("sujet-selected")
-sujet_selected.addEventListener('change', selecttheme);
 
 
 
@@ -114,57 +218,4 @@ function gouser() {
         window.location.href='user.html';
     }, 700);
     
-  }
-
-  function alltweets(){
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET","http://127.0.0.1:5000/tweeter");
-    xhr.onload = () => {
-        if (xhr.status == 200) {
-           for(let i=0;i<xhr.response.length;i++){
-            var newtweet = document.createElement("div");
-            newtweet.setAttribute("id", "tweet")
-            document.getElementById("tweets").appendChild(newtweet)
-
-            var newuser = document.createElement("div");
-            newuser.setAttribute("id", "user")
-            newtweet.appendChild(newuser)
-
-            var newuserphoto =  document.createElement("img");
-            newuserphoto.setAttribute("id", "photo-auteur")
-            newuserphoto.src = "user-photo.png";
-            newuser.appendChild(newuserphoto)
-
-            var newusername = document.createElement("p");
-            newusername.setAttribute("id", "auteur")
-            newusername.innerText = "profil";                   // Pouvoir recuperer via redis le profil ayant tweeté
-            newuser.appendChild(newusername)
-
-            var newtweetcorps = document.createElement("p");
-            newtweetcorps.setAttribute("id", "corps")
-            newtweetcorps.innerText = "corps";                  // Pouvoir recuperer via redis le corps du tweet
-            newtweet.appendChild(newtweetcorps)
-
-            var buttons = document.createElement("div");
-            buttons.setAttribute("class", "buttons")
-            newtweet.appendChild(buttons)
-
-            var likebutton = document.createElement("button");
-            likebutton.setAttribute("id", "like")
-            likebutton.innerText = "❤️";
-            buttons.appendChild(likebutton)
-
-            var retweetbutton = document.createElement("button");
-            retweetbutton.setAttribute("id", "retweet")
-            retweetbutton.innerText = "🔁";
-            buttons.appendChild(retweetbutton)
-
-            console.log(xhr.responseText)
-        }
-    }
-    else {
-        console.log(`Error: ${xhr.status}`);
-    }
-    };
-    xhr.send();
-  }
+}
--- Twitter/frontend/user.js
+++ Twitter/frontend/user.js
@@ -1,12 +1,14 @@
-profil = localStorage.getItem("username");
+var profil = localStorage.getItem("username");
 document.getElementById("user-connected").innerText =profil;
-
-function tweets(){
 var xhr = new XMLHttpRequest();
-xhr.open("GET","http://127.0.0.1:5000/tweeter/"+ profil);
-  xhr.onload = () => {
-    if (xhr.status === 200) {
-      /*console.log("200 !!!")
+xhr.open("POST","http://127.0.0.1:5000/"+profil);
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+xhr.onload = () => {
+if (xhr.status === 200) {
+  var tweets = JSON.parse(xhr.response)
+  for(i=0;i<tweets.length;i++){
+    if(tweets[i].profil==localStorage.getItem("username")){
       var newtweet = document.createElement("div");
       newtweet.setAttribute("id", "tweet")
       document.getElementById("tweets").appendChild(newtweet)
@@ -22,26 +24,25 @@ xhr.open("GET","http://127.0.0.1:5000/tweeter/"+ profil);
 
       var newusername = document.createElement("p");
       newusername.setAttribute("id", "auteur")
-      newusername.innerText = "zzz";
+      newusername.innerText = tweets[i].profil;
       newuser.appendChild(newusername)
 
       var newtweettheme = document.createElement("p");
       newtweettheme.setAttribute("id", "sujet")
-      newtweettheme.innerText = "#"+"sujet";
+      newtweettheme.innerText = "#"+tweets[i].sujet;
       newuser.appendChild(newtweettheme)
 
       var newtweetcorps = document.createElement("p");
       newtweetcorps.setAttribute("id", "corps")
-      newtweetcorps.innerText = "deded";
-      newtweet.appendChild(newtweetcorps)*/
-      console.log(xhr.responseText)
+      newtweetcorps.innerText = tweets[i].corps;
+      newtweet.appendChild(newtweetcorps)
+    }
   }
-  else {
-    console.log(`Error: ${xhr.status}`);
-  }
-};
-xhr.send();
 }
+else {
+  console.log(`Error: ${xhr.status}`);
+}};
+xhr.send();
 
 const bodyElements = document.querySelectorAll('body *');
     for (let i = 0; i < bodyElements.length; i++) {
